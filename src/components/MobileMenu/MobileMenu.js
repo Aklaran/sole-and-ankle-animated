72c8aa7083497dcb8787e86509e33e2448ad6486
aklaran <botbunk@gmail.com>
2023-05-03 08:35:36 -0700

    modal anim

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components/macro';
-import { DialogOverlay, DialogContent } from '@reach/dialog';
+import React from "react";
+import styled from "styled-components/macro";
+import { DialogOverlay, DialogContent } from "@reach/dialog";
 
-import { QUERIES, WEIGHTS } from '../../constants';
+import { QUERIES, WEIGHTS } from "../../constants";
 
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
   return (
@@ -37,12 +37,23 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
 };
 
 const Overlay = styled(DialogOverlay)`
+  @keyframes bg {
+    from {
+      background: var(--color-backdrop-initial);
+    }
+    to {
+      background: var(--color-backdrop);
+    }
+  }
+
+  animation: bg 500ms;
+  animation-fill-mode: forwards;
+
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
-  background: var(--color-backdrop);
   display: flex;
   justify-content: flex-end;
 `;
@@ -54,6 +65,37 @@ const Content = styled(DialogContent)`
   padding: 24px 32px;
   display: flex;
   flex-direction: column;
+
+  @media (prefers-reduced-motion: no-preference) {
+    @keyframes slide-in {
+      from {
+        transform: translateX(100%);
+      }
+      to {
+        transform: translateX(0%);
+      }
+    }
+
+    animation: slide-in 1000ms cubic-bezier(0.25, 0.1, 0.25, 1);
+    animation-fill-mode: forwards;
+
+    & > * {
+      opacity: 0%;
+
+      @keyframes fade-in {
+        from {
+          opacity: 0%;
+        }
+        to {
+          opacity: 100%;
+        }
+      }
+
+      animation: fade-in 1000ms ease;
+      animation-fill-mode: both;
+      animation-delay: 500ms;
+    }
+  }
 `;
 
 const CloseButton = styled(UnstyledButton)`
@@ -67,6 +109,7 @@ const Nav = styled.nav`
   display: flex;
   flex-direction: column;
   gap: 16px;
+  }
 `;
 
 const NavLink = styled.a`
